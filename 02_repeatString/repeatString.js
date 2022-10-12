36e7058a8f898edf0dc7b3e003b64cf9c49d09dd
sven <sven.ambrosius100@gmail.com>
2022-10-12 12:43:20 +0200

    finished task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let result = '';
+    if (text === '' || text === "") {
+        return ''
+    }
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++) {
+        result += text
+    }
+    return result
 };
 
 // Do not edit below this line
