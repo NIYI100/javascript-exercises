0a1d22446989797754511c0eae9268980cee8a36
sven <sven.ambrosius100@gmail.com>
2022-10-12 13:22:54 +0200

    finished task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let dividableByFour = (year % 4) === 0
+    let dividableByHundred = (year % 100) === 0
+    let dividableByFourHundred = (year % 400) === 0
 
+    return dividableByFour && (!dividableByHundred || (dividableByHundred && dividableByFourHundred))
 };
 
 // Do not edit below this line
