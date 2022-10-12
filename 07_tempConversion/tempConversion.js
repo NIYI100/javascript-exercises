baf544c8dfa7ac8c26493f2247a68ea151a3dc27
sven <sven.ambrosius100@gmail.com>
2022-10-12 13:30:39 +0200

    finished task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+  let tempInCelsius = ((tempInFahrenheit - 32) * (5/9))
+  return Math.round(tempInCelsius * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(tempInCelsius) {
+  let tempInFahrenheit = (tempInCelsius * (9/5) + 32)
+  return Math.round(tempInFahrenheit * 10) / 10
 };
 
 // Do not edit below this line
